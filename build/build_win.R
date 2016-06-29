e8ab07aaee5623222da444bd1f2e926a9b170604
vnijs <radiant@rady.ucsd.edu>
2016-06-29 15:46:33 -0700

    build fix for inst/rstudio

--- build/build_win.R
+++ build/build_win.R
@@ -1,3 +1,8 @@
 ## build for windows
-devtools::install("~/gh/radiant.multivariate")
-devtools::build("~/gh/radiant.multivariate", binary = TRUE)
+app <- "radiant.multivariate"
+path <- "../"
+devtools::install(file.path(path, app))
+f <- devtools::build(file.path(path, app))
+curr <- getwd(); setwd(path)
+system(paste0("R CMD INSTALL --build ", f))
+setwd(curr)
