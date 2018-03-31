7f0ae348255406a08045fd7675dcd14f183727c0
vnijs <radiant@rady.ucsd.edu>
2018-03-30 23:14:21 -0700

    launch in Rstudio window

--- R/radiant.R
+++ R/radiant.R
@@ -1,4 +1,4 @@
-#' Launch radiant.multivariate in default browser
+#' Launch radiant.multivariate in the default browser
 #'
 #' @details See \url{https://radiant-rstats.github.io/docs} for documentation and tutorials
 #'
@@ -10,6 +10,20 @@
 #' }
 #' @export
 radiant.multivariate <- function() radiant.data::launch(package = "radiant.multivariate", run = "browser")
+
+#' Launch radiant.multivariate in an Rstudio window
+#'
+#' @details See \url{https://radiant-rstats.github.io/docs} for documentation and tutorials
+#'
+#' @importFrom radiant.data launch
+#'
+#' @examples
+#' \dontrun{
+#' radiant.multivariate_window()
+#' }
+#' @export
+radiant.multivariate_window <- function() radiant.data::launch(package = "radiant.multivariate", run = "window")
+
 
 #' Launch radiant.multivariate in the Rstudio viewer
 #'
